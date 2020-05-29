9eba2383b81a7af181e6cbb61bd202029da69129
lucia-gomez <isabel.lucia.gomez@gmail.com>
2020-05-28 23:21:42 -0700

    only try new NLP search if different from original search

--- src/js/search.js
+++ src/js/search.js
@@ -35,9 +35,11 @@ export async function search(history) {
   results.setCurrentQuery(input)
   await searchSources(input, results, history)
   if (results.isEmpty()) {
-    input = processNLP(input)
-    results.newSearch(input)
-    await searchSources(input, results, history)
+    const newInput = processNLP(input)
+    if (newInput !== input) {
+      results.newSearch(input)
+      await searchSources(input, results, history)
+    }
   }
   results.finishedLoading()
   history.add(input)
